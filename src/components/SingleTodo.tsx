49866a3462eaf0cc3de56d5dac5e7a31d50132e0
Rabab Shayra <rababshayradhruba@gmail.com>
2023-08-09 18:09:44 +0900

    complete baic functions

--- src/components/SingleTodo.tsx
+++ src/components/SingleTodo.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { Todo } from '../model';
 import {AiFillEdit,AiFillDelete} from "react-icons/ai";
 import {MdDone} from "react-icons/md";
@@ -21,11 +21,24 @@ const SingleTodo = ({todo,todos,setTodos}:Props) => {
     setTodos(todos.filter((todo)=>todo.id!==id));
   };
 
+  const handleEdit=(e:React.FormEvent,id:number)=>{
+    e.preventDefault();
+    setTodos(todos.map((todo)=>(
+      todo.id===id?{...todo,todo:editTodo}:todo
+    )))
+    setEdit(false);
+  }
+
+  const inputRef=useRef<HTMLInputElement>(null);
+  useEffect(()=>{
+    inputRef.current?.focus();
+  },[edit])
+
   return (
-    <form className='todos__single'>
+    <form className='todos__single' onSubmit={(e)=>handleEdit(e,todo.id)}>
       {
         edit?(
-          <input value={editTodo} onChange={(e)=>setEditTodo(e.target.value)} className='todo__single--test'/>
+          <input ref={inputRef} value={editTodo} onChange={(e)=>setEditTodo(e.target.value)} className='todo__single--text'/>
         ):(
           todo.isDone?(
             <s className='todos__single--text'>{todo.todo}</s>
